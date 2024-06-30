054b13799107cf44dd47f8aa304534006e4576f4
zulfikar4568 <isnaen70@gmail.com>
2024-06-30 22:00:03 +0700

    fix: missing sites module

--- src/modules/registration.module.ts
+++ src/modules/registration.module.ts
@@ -2,8 +2,9 @@ import { PermissionModule } from '@/modules/permissions/permission.module';
 import RoleModule from '@/modules/roles/role.module';
 import UserModule from '@/modules/users/user.module';
 import { Module } from '@nestjs/common';
+import SiteModule from '@/modules/sites/site.module';
 
 @Module({
-  imports: [UserModule, RoleModule, PermissionModule],
+  imports: [UserModule, RoleModule, PermissionModule, SiteModule],
 })
 export class RegistrationModule {}
